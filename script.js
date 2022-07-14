a5a334cb1ac474deba8d957d067136a66071481c
Barbara Guimaraes <bahcamargo21@gmail.com>
2022-07-13 21:39:08 -0300

    corrigido itens faltantes no código

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-import prompt from "prompt-sync"
+import prompt from "prompt-sync";
 
 const resp = prompt();
 
 const listaOrdemAlf = () => {
     let prop = resp("Digite uma propriedade de CSS:");
     let array = [];
-    while (prop.toLowerCase() != "Sair") {
+    while (prop.toLowerCase() != "sair") {
         array.push(prop);
         prop = resp("Digite uma propriedade de CSS:")
     }
